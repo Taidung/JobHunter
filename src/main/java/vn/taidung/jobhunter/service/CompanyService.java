5f096ebc338bba84a8d1751341888842e1de94c4
Taidung <taidung3010@gmail.com>
2025-10-10 22:53:17 +0700

    pagination user + company

--- src/main/java/vn/taidung/jobhunter/service/CompanyService.java
+++ src/main/java/vn/taidung/jobhunter/service/CompanyService.java
@@ -3,9 +3,13 @@ package vn.taidung.jobhunter.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import vn.taidung.jobhunter.domain.Company;
+import vn.taidung.jobhunter.domain.dto.Meta;
+import vn.taidung.jobhunter.domain.dto.ResultPaginationDTO;
 import vn.taidung.jobhunter.repository.CompanyRepository;
 
 @Service
@@ -20,8 +24,20 @@ public class CompanyService {
         return this.companyRepository.save(c);
     }
 
-    public List<Company> handleGetCompany() {
-        return this.companyRepository.findAll();
+    public ResultPaginationDTO handleGetCompany(Pageable pageable) {
+        Page<Company> pageCompany = this.companyRepository.findAll(pageable);
+        ResultPaginationDTO rs = new ResultPaginationDTO();
+        Meta mt = new Meta();
+
+        mt.setPage(pageCompany.getNumber());
+        mt.setPageSize(pageCompany.getSize());
+
+        mt.setPages(pageCompany.getTotalPages());
+        mt.setTotal(pageCompany.getTotalElements());
+
+        rs.setMeta(mt);
+        rs.setResult(pageCompany.getContent());
+        return rs;
     }
 
     public Company handleUpdateCompany(Company c) {
